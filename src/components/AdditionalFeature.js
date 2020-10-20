4659cd8c7ee1ad63d0d1172008a9ad0212c13850
chqiu6 <chucky-qiu@lambdastudents.com>
2020-10-20 18:54:14 -0400

    set up provider, working on reducer

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -4,6 +4,7 @@ const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
+      {/* onClick ={() => props.} */}
       <button className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
---
+++ src/reducer/Reducer.js
@@ -0,0 +1,37 @@
+const state = {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image:
+        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 2, name: 'Racing detail package', price: 1500 },
+      { id: 3, name: 'Premium sound system', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+  };
+
+  export const featureReducer = (state, action) => {
+      switch(action.type){
+          case "ADD_FEATURES":
+              return {
+                  ...state,
+                  car : {...state.car, features: [...state.car.features, action.payload] }
+              }
+          case "REMOVE_FEATURES":
+              return {
+                  ...state, 
+                  car : {...state.car, features: state.car.features.filter(item => !item.features)}
+              }
+          default :
+          return state;
+      }
+  }
+
+  //!= immutability 
+  //splice 
+  
